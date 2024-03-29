b29b9b91b89788de1f8a022578a7f5381603c31b
Corben McClintock <s544086@nwmissouri.edu>
2024-03-29 12:02:27 -0500

    cleanup to make render work

--- routes/parent.js
+++ routes/parent.js
@@ -4,11 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('parent', { title: 'Search Results' });
-  class parent {
-    parent_type: String;
-    age: Number;
-    since: Number;
-  }
+  //class parent {
+    //parent_type: String;
+    //age: Number;
+    //since: Number;
+  //}
 });
 
 module.exports = router;
