7e0ea3128eb25f6237e3958d5e0db627694cf5d9
Corben McClintock <s544086@nwmissouri.edu>
2024-03-29 11:53:35 -0500

    Initializing repo

---
+++ routes/parent.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('parent', { title: 'Search Results' });
+  class parent {
+    parent_type: String;
+    age: Number;
+    since: Number;
+  }
+});
+
+module.exports = router;
