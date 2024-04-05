a0189edd5ee99bbce2b4d446a0b4cb780631ff34
Corben McClintock <s544086@nwmissouri.edu>
2024-04-05 16:48:43 -0500

    Completed Part 2 for Assignment 12

--- controllers/parents.js
+++ controllers/parents.js
@@ -35,8 +35,21 @@ exports.parent_delete = function(req, res) {
     res.send("NOT IMPLEMENTED: Parent delete DELETE " + req.params.id);
 };
 
-exports.parent_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Parent update PUT' + req.params.id);
+exports.parent_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify.req.body}`);
+    try {
+        let toUpdate = await Parent.findById(req.params.id);
+        if(req.body.parent_type)
+            toUpdate.parent_type = req.body.parent_type;
+        if(req.body.age) toUpdate.age = req.body.age;
+        if(req.body.since) toUpdate.since = req.body.since;
+        let result = await toUpdate.save();
+        console.log("Success" +result);
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed}`);
+    };
 };
 
 exports.parent_list = async function(req, res) {
