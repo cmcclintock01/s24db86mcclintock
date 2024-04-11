bac7dcc80a4612a1945fa13207a3b6ddefba3b3a
Corben McClintock <s544086@nwmissouri.edu>
2024-04-10 21:11:39 -0500

    Completed Part 5

--- routes/parent.js
+++ routes/parent.js
@@ -4,6 +4,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', parent_controllers.parent_view_all_Page);
+/* Get one parent page*/
 router.get('/detail', parent_controllers.parent_view_one_Page);
+/*Get create parent page*/
+router.get('/create', parent_controllers.parent_create_Page);
+
+
+
+
 module.exports = router;
+
 
