6e9d270d92daf5e855d2ad37aafa06d3a6ee731b
Corben McClintock <s544086@nwmissouri.edu>
2024-04-17 20:37:01 -0500

    Completed Part 4

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({username: username})
+      .then(function (user){
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, {message: 'Incorrect username.'});
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err){
+        return done(err)
+      })
+    })
+  )
+
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -11,6 +33,7 @@ mongoose.connect(connectionString);
 
 //Get the default connection
 var db = mongoose.connection;
+
 
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -62,6 +85,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -70,6 +100,12 @@ app.use('/parent', parentRouter);
 app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
 app.use('/resource', resourceRouter);
+
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
